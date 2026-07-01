bfe9115b616684a5d11ef1bad8c0d834eb8d31d6
buapebot <119761697+buapebot@users.noreply.github.com>
2026-07-01 16:21:36 +0000

    chore: update catty to v0.1.0

---
+++ Formula/catty.rb
@@ -0,0 +1,29 @@
+class Catty < Formula
+  desc "Personal assistant agent harness for Discord"
+  homepage "https://github.com/buape/catty"
+  version "0.1.0"
+  license "MIT"
+
+  on_macos do
+    if Hardware::CPU.arm?
+      url "https://github.com/buape/catty/releases/download/v0.1.0/catty-0.1.0-darwin-arm64.tar.gz"
+      sha256 "63b1699927b48d9586b994614f7499c5ffc66042cc10df0daa9ebc4c8e9710b2"
+    else
+      url "https://github.com/buape/catty/releases/download/v0.1.0/catty-0.1.0-darwin-amd64.tar.gz"
+      sha256 "20189f90f1cdbf688aa6ddb36d9645ffc79ea3177bf617b4c469031b676b09b5"
+    end
+  end
+
+  on_linux do
+    url "https://github.com/buape/catty/releases/download/v0.1.0/catty-0.1.0-linux-amd64.tar.gz"
+    sha256 "0afa17bb9f1c200db87b1b5d264b19e23122f4c8d285615aa10487f821f881de"
+  end
+
+  def install
+    bin.install "catty"
+  end
+
+  test do
+    assert_path_exists bin/"catty"
+  end
+end
