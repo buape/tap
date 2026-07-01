7df2a35231bd77ae5e6ff868a0fe1bfabfce3aab
Shadow <hi@shadowing.dev>
2026-07-01 11:30:10 -0500

    fix: use valid platform blocks for catty

--- Formula/catty.rb
+++ Formula/catty.rb
@@ -5,18 +5,22 @@ class Catty < Formula
   license "MIT"
 
   on_macos do
-    if Hardware::CPU.arm?
+    on_arm do
       url "https://github.com/buape/catty/releases/download/v0.1.0/catty-0.1.0-darwin-arm64.tar.gz"
       sha256 "63b1699927b48d9586b994614f7499c5ffc66042cc10df0daa9ebc4c8e9710b2"
-    else
+    end
+
+    on_intel do
       url "https://github.com/buape/catty/releases/download/v0.1.0/catty-0.1.0-darwin-amd64.tar.gz"
       sha256 "20189f90f1cdbf688aa6ddb36d9645ffc79ea3177bf617b4c469031b676b09b5"
     end
   end
 
   on_linux do
-    url "https://github.com/buape/catty/releases/download/v0.1.0/catty-0.1.0-linux-amd64.tar.gz"
-    sha256 "0afa17bb9f1c200db87b1b5d264b19e23122f4c8d285615aa10487f821f881de"
+    on_intel do
+      url "https://github.com/buape/catty/releases/download/v0.1.0/catty-0.1.0-linux-amd64.tar.gz"
+      sha256 "0afa17bb9f1c200db87b1b5d264b19e23122f4c8d285615aa10487f821f881de"
+    end
   end
 
   def install
